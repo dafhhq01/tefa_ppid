94463cd9128532b1464355168ece23b526a7d4cb
dafhhq01 <dafhhq01@gmail.com>
2026-07-22 23:20:17 +0700

    [BE5] add public service endpoints to web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\PublicServicesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/permohonan-informasi', [PublicServicesController::class, 'storeRequest'])->name('public.service.request.store');
+Route::post('/pengaduan', [PublicServicesController::class, 'trackComplaint'])->name('public.service.complaint.store');
+Route::get('/tracking{ticket}', [PublicServicesController::class, 'trackRequest'])->name('public.service.tracking.request');
+Route::get('/tracking-pengaduan{ticket}', [PublicServicesController::class, 'trackComplaint'])->name('public.service.tracking.complaint');
