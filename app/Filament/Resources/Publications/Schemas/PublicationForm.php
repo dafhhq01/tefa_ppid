85cf7198109d13c21e4c8da3339d19a6be592d19
Fauzan <fauzanzha09@gmail.com>
2026-07-23 12:44:58 +0700

    [BE4] Menambahkan CRUD filament admin

---
+++ app/Filament/Resources/Publications/Schemas/PublicationForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Publications\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class PublicationForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('file')
+                    ->required(),
+                DateTimePicker::make('published_at'),
+            ]);
+    }
+}
